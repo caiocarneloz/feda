e2d939d2f1b4be5b813b7c9161302fbc1318f500
caiocarneloz <caiocarneloz@gmail.com>
2019-05-16 17:06:09 -0300

    types

--- feda.py
+++ feda.py
@@ -1,20 +1,39 @@
+import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-plt.style.use('bmh')
+def initialize():
+    plt.style.use('bmh')
 
 def count_null(df):
-    columns = df.isnull().sum(axis = 0).sort_values(ascending=False)
-    columns = columns[columns != 0]
+    table = pd.DataFrame()
     
-    print(columns.to_string())
+    report = df.isnull().sum(axis = 0)[df.isnull().sum(axis = 0) != 0].sort_values(ascending=False)
     
-
-def distribuition(df, target):
+    table['Column'] = list(report.index)
+    table['Count'] = list(report.values)
+    
+    print(table.to_string())
+    
+def distribution(df, target):
     plt.figure(figsize=(9, 8))
     sns.distplot(df[target], color='g', bins=100, hist_kws={'alpha': 0.4})
     
-
-def get_types(df):
+def list_types(df):
     return list(set(df.dtypes.tolist()))
 
+def list_columns_by_type(df):
+    types = list_types(df)
+    
+    for type_ in types:
+        print(' - ' + str(type_))
+        print(list(df.select_dtypes(include=[type_]).columns))
+
+#initialize()
+#
+#df = pd.read_csv('/home/caiocarneloz/Dropbox/BCC/DataScience/Kaggle/house_prices/train.csv')
+#
+#count_null(df)
+#distribution(df,'SalePrice')
+#list_types(df)
+#list_columns_by_type(df)
